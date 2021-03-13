cf62766138dd118b664eb9060948cee7e8fd42b0
karankumarshreds <naileditnerd@gmail.com>
2021-03-13 19:39:18 +0530

    wired up marketing react code

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Mount function to render all the data onto index.html
+const mount = (el) => {
+  ReactDOM.render(<h1>Marketing</h1>, el);
+};
+
+// If we are in the development mode AND in isolation
+// call the mount funtion immediately
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#_marketing-dev-root');
+  // make sure we are running in isolation
+  if (el) {
+    mount(el);
+  }
+}
+
+// Otherwise, we would assume that we are being run by the
+// container and we will export this mount function for the
+// contianer
+export { mount };
