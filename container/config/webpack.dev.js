02b49c0a145069ae8cc0877424e27b8084c18f91
karankumarshreds <naileditnerd@gmail.com>
2021-03-13 22:41:07 +0530

    add container boilerplate

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,28 @@
+// helps you merge two different webpack configs
+const { merge } = require('webpack-merge');
+// takes our html file and adds script tags to it
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+// config for the development environment
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    //
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+};
+
+// this merges both configs
+// sequence matters, coinciding config elements
+// will be used for the later config
+// ie. in this case === devConfig
+module.exports = merge(commonConfig, devConfig);
