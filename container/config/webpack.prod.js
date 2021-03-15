84013e828f3874fa55b52f83ca9194f8bf4d8a03
karankumarshreds <naileditnerd@gmail.com>
2021-03-15 23:21:26 +0530

    webpack public path setting

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -14,6 +14,7 @@ const prodConfig = {
     // name + hash value (to overcome caching issue)
     filename: '[name].[contenthash].js',
   },
+  publicPath: '/container/latest/',
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
@@ -29,3 +30,17 @@ const prodConfig = {
 };
 
 module.exports = merge(commonConfig, prodConfig);
+
+/**
+ * @publicPath
+ * This will make sure that html plugin appends this
+ * path before adding all the script tags to the index.html
+ * so that we load up the correct js files from the container
+ * folder from the s3 bucket
+ * If not done, it will just add the domain name (bucket/cdn url)\
+ * which would try and load the main.js(webpack file) from the
+ * root of the s3 bucket and throw an error :
+ * ***********************************************
+ * Uncaught SyntaxError: Unexpected token '<'
+ * ***********************************************
+ */
