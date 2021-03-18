aec0761137a9a7267b64ad0cefb7faa4b47af990
karankumarshreds <naileditnerd@gmail.com>
2021-03-18 21:42:01 +0530

    add header with css

--- container/src/App.js
+++ container/src/App.js
@@ -1,1 +1,15 @@
-// test commit //
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
+};
+
+export default App;
