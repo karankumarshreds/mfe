f1b72319d3500a842361e2694bd7e86e1f526ecb
karankumarshreds <naileditnerd@gmail.com>
2021-03-18 22:34:54 +0530

    randomised prod classname

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+/**
+ * This makes sure that all the elements using inline styles
+ * have their classnames (which are randomly generated) by
+ * material ui for styling start with 'ma' such that they do
+ * not collide (className collision) with any other projct
+ * when they are consumed by the consumer
+ */
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cont',
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- marketing/src/components/App.js
+++ marketing/src/components/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './Landing';
 import Pricing from './Pricing';
 
+/**
+ * This makes sure that all the elements using inline styles
+ * have their classnames (which are randomly generated) by
+ * material ui for styling start with 'ma' such that they do
+ * not collide (className collision) with any other projct
+ * when they are consumed by the consumer
+ */
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 const App = () => {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={Pricing} />
